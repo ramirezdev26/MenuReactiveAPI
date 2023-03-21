5b56560a986470c3eb2fdc369f9e15e44e051a8e
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-21 18:38:53 -0500

    Added the configuration of Model Mapper and the Null Parameter Exception

---
+++ src/main/java/co/com/menu/reactive/api/MenuReactivveAPI/errorHandler/NullParameterException.java
@@ -0,0 +1,7 @@
+package co.com.menu.reactive.api.MenuReactivveAPI.errorHandler;
+
+public class NullParameterException extends RuntimeException {
+    public NullParameterException(String message) {
+        super(message);
+    }
+}
