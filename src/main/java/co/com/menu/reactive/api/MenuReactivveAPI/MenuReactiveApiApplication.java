29f6d83c7c304c6edf8fcdd6b3428215e0697550
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-21 17:05:35 -0500

    Set all the architecture of the system, added the dependencies needed.

---
+++ src/main/java/co/com/menu/reactive/api/MenuReactivveAPI/MenuReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.menu.reactive.api.MenuReactivveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/menu/reactive/api/MenuReactivveAPI/MenuReactivveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.menu.reactive.api.MenuReactivveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuReactivveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
